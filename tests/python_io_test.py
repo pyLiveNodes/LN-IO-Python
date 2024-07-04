20f7ebf8ed79079542aeb5c687b64ed7b7821283
Dennis Müller <de_br@uni-bremen.de>
2024-07-04 08:30:16 -0700

    Add test cases from original in livenodes_core_nodes

--- tests/python_io_test.py
+++ tests/python_io_test.py
@@ -22,19 +22,31 @@ def run_single_test(data):
 
     actual = np.array(out_python.get_state())
 
-    np.testing.assert_almost_equal(data, actual)
+    np.testing.assert_equal(data, actual)
 
 
 class TestProcessing:
 
-    def test_1D(self):
+    def test_list(self):
+        run_single_test(list(range(100)))
+
+    def test_numpy_1D(self):
         run_single_test(np.arange(100))
 
-    def test_2D(self):
+    def test_numpy_2D(self):
         run_single_test(np.arange(100).reshape((20, 5)))
 
-    def test_2D_nested(self):
+    def test_numpy_2D_nested_axis_0(self):
         run_single_test(np.arange(100).reshape((1, 20, 5)))
 
-    def test_3D(self):
+    def test_numpy_2D_nested_axis_1(self):
+        run_single_test(np.arange(100).reshape((20, 1, 5)))
+
+    def test_numpy_2D_nested_axis_2(self):
+        run_single_test(np.arange(100).reshape((20, 5, 1)))
+
+    def test_numpy_3D(self):
         run_single_test(np.arange(100).reshape((2, 10, 5)))
+
+    def test_large(self):
+        run_single_test(np.arange(1000000).reshape((1000, -1)))
